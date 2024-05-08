454c2e8762d207965b3117bc2cd0e93b03b4a3c0
Darren <121430748+LezyPanda@users.noreply.github.com>
2024-05-08 12:58:13 +1200

    LFSR implementation

---
+++ src/main/D_H/LSFRTB.vhd
@@ -0,0 +1,40 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY Testbench IS
+END Testbench;
+
+ARCHITECTURE TBarch OF Testbench IS
+  COMPONENT LFSR8 IS
+    PORT (Clk, Rst: IN std_logic;
+          output: OUT std_logic_vector (8 DOWNTO 0));
+  END COMPONENT;
+
+  SIGNAL Clk_s, Rst_s: std_logic;
+  SIGNAL output_s: std_logic_vector(8 DOWNTO 0);
+
+BEGIN
+  CompToTest: LFSR8 PORT MAP (Clk_s, Rst_s, output_s);
+   
+  Clk_proc: PROCESS
+  BEGIN
+    Clk_s <= '1';
+    WAIT FOR 10 ns;
+    Clk_s <= '0';
+    WAIT FOR 10 ns;
+  END PROCESS clk_proc;
+                      
+  Vector_proc: PROCESS
+  BEGIN
+    Rst_s <= '1';
+    WAIT FOR 5 NS;
+    Rst_s <= '0';
+    FOR index IN 0 To 4 LOOP
+      WAIT UNTIL Clk_s='1' AND Clk_s'EVENT;
+    END LOOP;
+    WAIT FOR 5 NS;
+  --  ASSERT output_s = X"88" REPORT "Failed output=88";
+    WAIT;
+  END PROCESS Vector_proc;
+
+END TBarch;
