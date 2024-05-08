3eed9a980a93a32e6564a8aeae4377e1cdefad84
Darren <121430748+LezyPanda@users.noreply.github.com>
2024-05-08 12:05:20 +1200

    lkasjljsdaljsdlkj

--- src/main/D_H/DP_CLK_sim/cadence/ncsim_setup.sh
+++ src/main/D_H/DP_CLK_sim/cadence/ncsim_setup.sh
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 18.1 625 win32 2024.05.07.21:59:39
+# ACDS 18.1 625 win32 2024.05.08.12:00:05
 
 # ----------------------------------------
 # ncsim - auto-generated simulation script
@@ -106,12 +106,12 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 18.1 625 win32 2024.05.07.21:59:39
+# ACDS 18.1 625 win32 2024.05.08.12:00:05
 # ----------------------------------------
 # initialize variables
 TOP_LEVEL_NAME="DP_CLK"
 QSYS_SIMDIR="./../"
-QUARTUS_INSTALL_DIR="C:/intelfpga_lite/18.1/quartus/"
+QUARTUS_INSTALL_DIR="C:/intelfpga/18.1/quartus/"
 SKIP_FILE_COPY=0
 SKIP_DEV_COM=0
 SKIP_COM=0
