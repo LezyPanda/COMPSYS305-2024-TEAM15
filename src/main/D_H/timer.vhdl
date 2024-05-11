14b554981dcb988c127f97fe270ec03f5f73b840
Thomas Yeh <cyeh243@aucklanduni.ac.nz>
2024-05-12 02:52:18 +1200

    TIEMR

--- src/main/D_H/timer.vhdl
+++ src/main/D_H/timer.vhdl
@@ -4,9 +4,8 @@ use IEEE.std_logic_unsigned.all;
 
 entity timer is
 	port(
-		CLOCK_50 : in std_logic; -- Clock
+		clock50 : in std_logic; -- Clock
 		KEY0 : in std_logic; -- Start
-		SW : in std_logic_vector(9 downto 0); -- Data in
 		timeOut : out std_logic;
 		HEX2 : out std_logic_vector(6 downto 0); -- m
 		HEX1 : out std_logic_vector(6 downto 0); --st
@@ -17,66 +16,55 @@ entity timer is
 end entity timer;
 
 architecture atimer of timer is
-    component bcd_counter is
+    component BCDCounter is
         port (
             clock, reset, enable : in std_logic;
-		minNum, maxNum : in std_logic_vector(3 downto 0);
-		count : out std_logic_vector(3 downto 0)
+				minNum, maxNum : in std_logic_vector(3 downto 0);
+				count : out std_logic_vector(3 downto 0)
         );
     end component;
 
-	component Clk_divider is
-		port (
-			 port (
-        clockIn, reset : in std_logic;
+	 component ClockDivider is
+		  port (
+		  clockIn, reset : in std_logic;
 			factor : in integer; -- How many input periods to represent one output period, i.e. 4 means it will take 3 input period to have a complete output waveform
         clockOut: out std_logic
     );
-		);
 	end component;
 
-	component BCD_to_SevenSeg is
-		 port (BCD_digit : in std_logic_vector(3 downto 0);
-			   SevenSeg_out : out std_logic_vector(6 downto 0));
+	component BCD2SevenSeg is
+	port (
+		DisplayDigit : in std_logic_vector(3 downto 0);
+		SevenSeg : out std_logic_vector(6 downto 0)
+				);
 	end component;
 
-	signal direction, init : std_logic;
+	signal init : std_logic;
     signal vEnable1, vEnable2, vEnable3 : std_logic;
     signal vQ1, vQ2, vQ3 : std_logic_vector(3 downto 0);
-	signal vInitNum1, vInitNum2, vInitNum3 : std_logic_vector(3 downto 0);
-	signal vMaxNum1, vMaxNum2, vMaxNum3 : std_logic_vector(3 downto 0);
-	signal vMinNum1, vMinNum2, vMinNum3 : std_logic_vector(3 downto 0);
 	signal vClk, Reset : std_logic;
 begin
-	dummy : Clk_divider port map(CLOCK_50,50000,Reset, vClk);
-	dummy2 : bcd_counter port map(vClk, init, vEnable1, direction, vInitNum1, vMaxNum1, vMinNum1, vQ1); -- m
-    dummy3 : bcd_counter port map(vClk, init, vEnable2, direction, vInitNum2, vMaxNum2, vMinNum2, vQ2); -- st
-    dummy4 : bcd_counter port map(vClk, init, vEnable3, direction, vInitNum3, vMaxNum3, vMinNum3, vQ3); -- so
-	dummy5 : BCD_to_SevenSeg port map(vQ1, HEX2);
-	dummy6 : BCD_to_SevenSeg port map(vQ2, HEX1);
-	dummy7 : BCD_to_SevenSeg port map(vQ3, HEX0);
+-- 50000000 to 1 s
+	dummy : ClockDivider port map(clock50, Reset, 10000000, vClk);
+	dummy2 : BCDCounter port map(vClk, init, vEnable1, "0000", "0011", vQ1); -- m
+    dummy3 : BCDCounter port map(vClk, init, vEnable2, "0000", "0101", vQ2); -- st
+    dummy4 : BCDCounter port map(vClk, init, vEnable3, "0000", "1001", vQ3); -- so
+	dummy5 : BCD2SevenSeg port map(vQ1, HEX2);
+	dummy6 : BCD2SevenSeg port map(vQ2, HEX1);
+	dummy7 : BCD2SevenSeg port map(vQ3, HEX0);
 	
     process (vClk, KEY0)
 		variable tEnable1, tEnable2, tEnable3 : std_logic; 
     begin
+		
         if (KEY0 = '0') then
 			Reset <= '1';
             init <= '1';
-			vInitNum1 <= "00" & SW(9 downto 8);
-			if (SW(7 downto 4) < "0111") then
-				vInitNum2 <= SW(7 downto 4);
-				vInitNum3 <= SW(3 downto 0);
-			else
-				vInitNum2 <= "0110";
-				vInitNum3 <= "0000";
-			end if;
-			vMaxNum1 <= "0011"; -- 3
-			vMaxNum2 <= "0101"; -- 5
-			vMaxNum3 <= "1001"; -- 9
-			vMinNum1 <= "0000";
-			vMinNum2 <= "0000";
-			vMinNum3 <= "0000";
+				tEnable1 := '1';
+				tEnable2 := '1';
+				tEnable3 := '1';
 			timeOut <= '0';
+			
         elsif (rising_edge(vClk)) then
 			Reset <= '0';
 			init <= '0';
@@ -88,24 +76,25 @@ begin
 				timeOut <= '1';
 				LEDR <= "1111111111";
 			-- if the second one is 9 second turn on the second ten seven seg
-			elsif (vQ3 = "1001") then
+			elsif (vQ3 = "1000") then
 				LEDR <= "0000000000";
-					-- if second sevenseg is 5 turn on the minute counter
+				-- if tens second sevenseg is 5 turn on the minute counter
 				if (vQ2 = "0101") then
-					tEnable1 := '0';
+					tEnable1 := '1';
 					tEnable2 := '1';
+					tEnable3 := '1';
 				 -- if 59 turn increase minute
 				else
-					tEnable1 := '1';
+					tEnable1 := '0';
 					tEnable2 := '1';
 					tEnable3 := '1';
 				end if;
 			-- else enable second again
 			else
 				LEDR <= "0000000000";
-				tEnable1 := '1';
+				tEnable1 := '0';
 				tEnable2 := '0';
-				tEnable3 := '0';
+				tEnable3 := '1';
 			end if;
 		else
 			Reset <= '0';
