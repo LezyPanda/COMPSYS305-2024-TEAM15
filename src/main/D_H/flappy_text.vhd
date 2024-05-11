112caf0160b2f15ccdc9f9c7ce0da359565fc247
Thomas Yeh <cyeh243@aucklanduni.ac.nz>
2024-05-11 22:32:20 +1200

    tEXT DISPLAY GAME

--- src/main/D_H/flappy_text.vhd
+++ src/main/D_H/flappy_text.vhd
@@ -5,58 +5,54 @@ USE IEEE.STD_LOGIC_UNSIGNED.all;
 
 ENTITY flappy_text IS
    PORT(
-		  signal pixel_column, pixel_row	: in std_logic_vector(9 downto 0);
-		  signal character_address					: out std_logic_vector(5 downto 0);
-		  signal font_row, font_col			: out std_logic_vector(2 downto 0));
+		  pixel_row, pixel_column	: in std_logic_vector(9 downto 0);
+		  character_address			: out std_logic_vector(5 downto 0);
+		  font_row, font_col			: out std_logic_vector(2 downto 0)
+	  );
 end flappy_text;
 
 architecture behavior of flappy_text is
 
-signal pixel_row_t, pixel_column_t			: std_logic_vector(9 downto 0);
-
-
-begin           
-pixel_column_t <= pixel_column;
-pixel_row_t <= pixel_row;
-
-TEXT_Display: process (pixel_column_t, pixel_row_t)
 begin
-		if (pixel_column_t >= CONV_STD_LOGIC_VECTOR(288, 10)) and
-			(pixel_column_t <= CONV_STD_LOGIC_VECTOR(304, 10)) and
-			(pixel_row_t >= CONV_STD_LOGIC_VECTOR(320, 10)) and
-			(pixel_row_t <= CONV_STD_LOGIC_VECTOR(335, 10)) then
-			font_row <= pixel_row_t(3 downto 1);
-			font_col <= pixel_column_t(3 downto 1);
-			character_address <= CONV_STD_LOGIC_VECTOR(7, 6);
-		--A
-		elsif (pixel_column_t >= CONV_STD_LOGIC_VECTOR(304, 10)) and
-			(pixel_column_t <= CONV_STD_LOGIC_VECTOR(320, 10)) and
-			(pixel_row_t >= CONV_STD_LOGIC_VECTOR(320, 10)) and
-			(pixel_row_t <= CONV_STD_LOGIC_VECTOR(335, 10)) then
-			font_row <= pixel_row_t(3 downto 1);
-			font_col <= pixel_column_t(3 downto 1);
-			character_address <= CONV_STD_LOGIC_VECTOR(1, 6);
-		--M
-		elsif (pixel_column_t >= CONV_STD_LOGIC_VECTOR(320, 10)) and
-			(pixel_column_t <= CONV_STD_LOGIC_VECTOR(336, 10)) and
-			(pixel_row_t >= CONV_STD_LOGIC_VECTOR(320, 10)) and
-			(pixel_row_t <= CONV_STD_LOGIC_VECTOR(335, 10)) then
-			font_row <= pixel_row_t(3 downto 1);
-			font_col <= pixel_column_t(3 downto 1);
-			character_address <= CONV_STD_LOGIC_VECTOR(13, 6);
-		--E
-		elsif (pixel_column_t >= CONV_STD_LOGIC_VECTOR(336, 10)) and
-			(pixel_column_t <= CONV_STD_LOGIC_VECTOR(352, 10)) and
-			(pixel_row_t >= CONV_STD_LOGIC_VECTOR(320, 10)) and
-			(pixel_row_t <= CONV_STD_LOGIC_VECTOR(335, 10)) then
-			font_row <= pixel_row_t(3 downto 1);
-			font_col <= pixel_column_t(3 downto 1);
-			character_address <= CONV_STD_LOGIC_VECTOR(5, 6);
-		else
-			font_row <= pixel_row_t(3 downto 1);
-			font_col <= pixel_column_t(3 downto 1);
-			character_address <= CONV_STD_LOGIC_VECTOR(32, 6);
-		end if;
+
+	TEXT_Display: process (pixel_column, pixel_row)
+	begin
+			--G
+			if (pixel_column >= CONV_STD_LOGIC_VECTOR(8, 10)) and
+				(pixel_column <= CONV_STD_LOGIC_VECTOR(8 + 8, 10)) and
+				(pixel_row >= CONV_STD_LOGIC_VECTOR(8, 10)) and
+				(pixel_row <= CONV_STD_LOGIC_VECTOR(8 + 8 - 1, 10)) then
+				character_address <= CONV_STD_LOGIC_VECTOR(7, 6);
+				font_row <= pixel_row(2 downto 0);
+				font_col <= pixel_column(2 downto 0);
+			--A
+			elsif (pixel_column >= CONV_STD_LOGIC_VECTOR(80, 10)) and
+				(pixel_column <= CONV_STD_LOGIC_VECTOR(80 + 16, 10)) and
+				(pixel_row >= CONV_STD_LOGIC_VECTOR(80, 10)) and
+				(pixel_row <= CONV_STD_LOGIC_VECTOR(80 + 16 - 1, 10)) then
+				character_address <= CONV_STD_LOGIC_VECTOR(1, 6);
+				font_row <= pixel_row(3 downto 1);
+				font_col <= pixel_column(3 downto 1);
+			--M
+			elsif (pixel_column >= CONV_STD_LOGIC_VECTOR(160, 10)) and
+				(pixel_column <= CONV_STD_LOGIC_VECTOR(160 + 32, 10)) and
+				(pixel_row >= CONV_STD_LOGIC_VECTOR(160, 10)) and
+				(pixel_row <= CONV_STD_LOGIC_VECTOR(160 + 32 - 1, 10)) then
+				character_address <= CONV_STD_LOGIC_VECTOR(13, 6);		
+				font_row <= pixel_row(4 downto 2);
+				font_col <= pixel_column(4 downto 2);
+			--E
+			elsif (pixel_column >= CONV_STD_LOGIC_VECTOR(256, 10)) and
+				(pixel_column <= CONV_STD_LOGIC_VECTOR(256 + 64, 10)) and
+				(pixel_row >= CONV_STD_LOGIC_VECTOR(256, 10)) and
+				(pixel_row <= CONV_STD_LOGIC_VECTOR(256 + 64 - 1, 10)) then
+				character_address <= CONV_STD_LOGIC_VECTOR(5, 6);				
+				font_row <= pixel_row(5 downto 3);
+				font_col <= pixel_column(5 downto 3);
+			else
+				character_address <= CONV_STD_LOGIC_VECTOR(32, 6);
+			end if;
+			
 	end process TEXT_Display;
 
 end behavior;
