2820d36010a991022fffad337a50feff479ba063
Thomas Yeh <cyeh243@aucklanduni.ac.nz>
2024-05-27 19:38:27 +1200

    lmao

--- src/main/D_H/timer.vhdl
+++ src/main/D_H/timer.vhdl
@@ -7,7 +7,6 @@ entity timer is
 		clock50 : in std_logic; -- Clock
 		Reset : in std_logic; -- Start
 		Enable : in std_logic;
-		timeOut : out std_logic;
 		HEX2 : out std_logic_vector(6 downto 0); -- m
 		HEX1 : out std_logic_vector(6 downto 0); --st
 		HEX0 : out std_logic_vector(6 downto 0); -- so
@@ -28,7 +27,6 @@ architecture atimer of timer is
 	 component ClockDivider is
 		  port (
 		  clockIn, reset : in std_logic;
-			factor : in integer; -- How many input periods to represent one output period, i.e. 4 means it will take 3 input period to have a complete output waveform
         clockOut: out std_logic
     );
 	end component;
@@ -45,7 +43,7 @@ architecture atimer of timer is
     signal vQ1, vQ2, vQ3 : std_logic_vector(3 downto 0);
 	signal vClk : std_logic;
 begin
-	dummy : ClockDivider port map(clock50, Reset, 25000000, vClk);
+	dummy : ClockDivider port map(clock50, Reset, vClk);
 	dummy2 : BCDCounter port map(vClk, init, vEnable1, "0000", "0011", vQ1); -- m
     dummy3 : BCDCounter port map(vClk, init, vEnable2, "0000", "0101", vQ2); -- st
     dummy4 : BCDCounter port map(vClk, init, vEnable3, "0000", "1001", vQ3); -- so
@@ -59,7 +57,6 @@ begin
 		
         if (Reset = '1') then
             init <= '1';
-			timeOut <= '0';
         elsif (rising_edge(vClk)) then
 			if (Enable = '1') then
 				init <= '0';
@@ -68,7 +65,6 @@ begin
 					tEnable1 := '0';
 					tEnable2 := '0';
 					tEnable3 := '0';
-					timeOut <= '1';
 					LEDR <= "1111111111";
 				-- if the second one is 9 second turn on the second ten seven seg
 				elsif (vQ3 = "1000") then
